d7e46dad4db6c8ef5ca20530954d1bf9e0b27322
unknown <jithinbp007@gmail.com>
2024-03-15 23:22:30 +0530

    updating responsiveness

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ import "./style.css";
 const input = document.getElementById("link-input");
 const linkForm = document.getElementById("link-form");
 const errMsg = document.getElementById("err-msg");
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
 
 function isValidUrl(url) {
   try {
@@ -12,6 +14,35 @@ function isValidUrl(url) {
     return true;
   } catch (e) {
     return false;
+  }
+}
+
+async function shortenUrl(longUrl) {
+  // const encodedUrl = encodeURIComponent(longUrl);
+
+  // 2. Define the API endpoint URL
+  const apiUrl = "https://cleanuri.com/api/v1/shorten";
+
+  // 3. Prepare the data object with the encoded long URL
+  const data = { url: longUrl };
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      body: JSON.stringify(data), // Convert data object to JSON string
+      headers: { "Content-Type": "application/json" }, // Set content type
+      mode: "no-cors",
+    });
+
+    if (!response.ok) {
+      throw new Error(`API request failed with status ${response.status}`);
+    }
+
+    const responseJson = await response.json();
+
+    return responseJson.result.short_url; // Extract the shortened URL from the response
+  } catch (error) {
+    console.error("Error shortening URL:", error);
+    return null; // Handle errors gracefully, like returning null
   }
 }
 
@@ -25,9 +56,22 @@ function formSubmit(e) {
     errMsg.innerHTML = "Please enter a valid URL";
     input.classList.add("border-red");
   } else {
+    const longUrl = input.value;
+    shortenUrl(longUrl).then((shortUrl) => {
+      if (shortUrl) {
+        console.log("Shortened URL:", shortUrl);
+      }
+    });
     errMsg.innerHTML = "";
     input.classList.remove("border-red");
   }
 }
 
+function handleMenuClick(e) {
+  menuBtn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
+
 linkForm.addEventListener("submit", formSubmit);
+menuBtn.addEventListener("click", handleMenuClick);
