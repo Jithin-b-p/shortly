a7051400b4a3a766ffcfb2bed4e043c431fe8fb5
unknown <jithinbp007@gmail.com>
2024-03-15 11:28:31 +0530

    adding header with navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: "hsl(var(--clr-cyan))",
+        darkViolet: "hsl(var(--clr-dark-violet))",
+        red: "hsl(var(--clr-red))",
+        clrNeutral: {
+          100: "hsl(var(--clr-neutral-100))",
+          200: "hsl(var(--clr-neutral-200))",
+          300: "hsl(var(--clr-neutral-300))",
+          400: "hsl(var(--clr-neutral-400))",
+        },
+      },
+      spacing: {
+        180: "32rem",
+      },
+    },
   },
   plugins: [],
 };
