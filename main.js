eb97dfe4429485f8e2818ddbec0a3631c966648f
unknown <jithinbp007@gmail.com>
2024-03-15 16:27:56 +0530

    adding feature boxes and CTA

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
+"use strict";
+
 import "./style.css";
+
+const input = document.getElementById("link-input");
+const linkForm = document.getElementById("link-form");
+const errMsg = document.getElementById("err-msg");
+
+function isValidUrl(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function formSubmit(e) {
+  e.preventDefault();
+
+  if (input.value === "") {
+    errMsg.innerHTML = "Please enter something";
+    input.classList.add("border-red");
+  } else if (!isValidUrl(input.value)) {
+    errMsg.innerHTML = "Please enter a valid URL";
+    input.classList.add("border-red");
+  } else {
+    errMsg.innerHTML = "";
+    input.classList.remove("border-red");
+  }
+}
+
+linkForm.addEventListener("submit", formSubmit);
